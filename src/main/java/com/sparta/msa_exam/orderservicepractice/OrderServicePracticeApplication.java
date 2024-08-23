eeecc3430f06c128f0b5d778234ce1e212e72ce9
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-08-23 16:04:53 +0900

    init : project

---
+++ src/main/java/com/sparta/msa_exam/orderservicepractice/OrderServicePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.msa_exam.orderservicepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServicePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServicePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/msa_exam/orderservicepractice/OrderServicePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.msa_exam.orderservicepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServicePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
