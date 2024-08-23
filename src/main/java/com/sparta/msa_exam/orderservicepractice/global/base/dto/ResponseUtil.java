c1d2e833daf62cc687c06ccc8a1ceee5d4dcfe10
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-08-23 17:45:37 +0900

    add : 공통 응답 처리 추가

---
+++ src/main/java/com/sparta/msa_exam/orderservicepractice/global/base/dto/ResponseUtil.java
@@ -0,0 +1,29 @@
+package com.sparta.msa_exam.orderservicepractice.global.base.dto;
+
+
+import com.sparta.msa_exam.orderservicepractice.global.base.exception.ErrorCode;
+
+public class ResponseUtil {
+
+	public static ResponseBody<Void> createSuccessResponse() {
+		return new SuccessResponseBody<>();
+	}
+
+	public static <T> ResponseBody<T> createSuccessResponse(T data) {
+		return new SuccessResponseBody<>(data);
+	}
+
+	public static ResponseBody<Void> createFailureResponse(ErrorCode errorCode) {
+		return new FailedResponseBody(
+			errorCode.getCode(),
+			errorCode.getMessage()
+		);
+	}
+
+	public static ResponseBody<Void> createFailureResponse(ErrorCode errorCode, String customMessage) {
+		return new FailedResponseBody(
+			errorCode.getCode(),
+			customMessage
+		);
+	}
+}
