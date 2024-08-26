a8f01c06778cf23240a9f6997109be32301adf31
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-08-26 14:37:16 +0900

    add : @SQLRestriction Region 도메인 검색 제약조건 적용

--- src/main/java/com/sparta/msa_exam/orderservicepractice/domain/region/domain/Region.java
+++ src/main/java/com/sparta/msa_exam/orderservicepractice/domain/region/domain/Region.java
@@ -1,11 +1,14 @@
 package com.sparta.msa_exam.orderservicepractice.domain.region.domain;
 
+import com.sparta.msa_exam.orderservicepractice.domain.region.domain.dto.request.CreateRegionRequest;
 import com.sparta.msa_exam.orderservicepractice.global.base.domain.BaseEntity;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.SQLRestriction;
 
 import java.util.UUID;
 
@@ -14,9 +17,21 @@ import java.util.UUID;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "regions")
+@SQLRestriction(value = "deleted_at is NULL")
 public class Region extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public Region(String name) {
+        this.name = name;
+    }
+
+    public static Region from(@Valid CreateRegionRequest request) {
+        return new Region(request.name());
+    }
 }
