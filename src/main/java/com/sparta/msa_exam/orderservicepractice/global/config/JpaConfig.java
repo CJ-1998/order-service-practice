a9871a6d3c91db39151590263ba370afdce0bc1d
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-08-26 14:29:48 +0900

    add : createdBy, updatedBy 자동화 (추후 수정 필요)

--- src/main/java/com/sparta/msa_exam/orderservicepractice/global/config/JpaConfig.java
+++ src/main/java/com/sparta/msa_exam/orderservicepractice/global/config/JpaConfig.java
@@ -1,9 +1,19 @@
 package com.sparta.msa_exam.orderservicepractice.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        // TODO. SecurityContext 에서 꺼내온 Authentication 에서 nickname 추출
+        return () -> Optional.of("닉네임");
+    }
 }
