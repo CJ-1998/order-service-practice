3a1fbf39a830392707112cd188ad5adcda06b27a
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-08-30 08:49:29 +0900

    add : Payment 도메인 설정
    Payment 도메인에 필요한 필드들 설정

--- src/main/java/com/sparta/msa_exam/orderservicepractice/domain/payment/domain/Payment.java
+++ src/main/java/com/sparta/msa_exam/orderservicepractice/domain/payment/domain/Payment.java
@@ -1,22 +1,53 @@
 package com.sparta.msa_exam.orderservicepractice.domain.payment.domain;
 
+import com.sparta.msa_exam.orderservicepractice.domain.payment.domain.enums.PaymentStatus;
+import com.sparta.msa_exam.orderservicepractice.domain.user.domain.User;
 import com.sparta.msa_exam.orderservicepractice.global.base.domain.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.UUID;
-
 @Slf4j
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "payments")
+@Table(name = "p_payment")
 public class Payment extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "payment_id", updatable = false, nullable = false)
     private UUID id;
+
+    @Column(name = "payment_amount", nullable = false)
+    private int paymentAmount;
+
+    @Column(name = "payment_at", nullable = false)
+    private LocalDate paymentAt;
+
+    @Column(name = "transaction_id", nullable = false)
+    private UUID transactionId;
+
+    @Column(name = "payment_status", nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
