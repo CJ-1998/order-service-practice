52100af03f921d0562033a6356b4a263fbc2269c
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-08-30 09:50:21 +0900

    add : AI 컨트롤러에 권한 체크 추가
    AI 컨트롤러 모든 요청 Admin, Owner만 접근할 수 있도록 설정

--- src/main/java/com/sparta/msa_exam/orderservicepractice/domain/ai/controller/AIController.java
+++ src/main/java/com/sparta/msa_exam/orderservicepractice/domain/ai/controller/AIController.java
@@ -5,11 +5,14 @@ import com.sparta.msa_exam.orderservicepractice.domain.ai.dto.AIRecordDto;
 import com.sparta.msa_exam.orderservicepractice.domain.ai.dto.AIRequestDto;
 import com.sparta.msa_exam.orderservicepractice.domain.ai.dto.AIResponseDto;
 import com.sparta.msa_exam.orderservicepractice.domain.ai.service.AIService;
+import com.sparta.msa_exam.orderservicepractice.domain.user.domain.UserRole;
+import com.sparta.msa_exam.orderservicepractice.domain.user.domain.UserRole.Authority;
 import com.sparta.msa_exam.orderservicepractice.global.base.dto.ResponseBody;
 import com.sparta.msa_exam.orderservicepractice.global.base.dto.ResponseUtil;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,17 +27,19 @@ public class AIController {
 
     private final AIService aiService;
 
+    @Secured({UserRole.Authority.ADMIN, Authority.OWNER}) // 관리자 및 가게 주인용
     @PostMapping("/request")
     public ResponseEntity<ResponseBody<AIResponseDto>> AIRequest(@RequestBody AIRequestDto aiRequestDto) {
-        // TODO. 권한 체크, 예외 처리
+        // TODO. 예외 처리
         String message = aiService.getAIRequest(aiRequestDto.getRequest());
         AIResponseDto aiResponseDto = new AIResponseDto(message);
         return ResponseEntity.ok(ResponseUtil.createSuccessResponse(aiResponseDto));
     }
 
+    @Secured({UserRole.Authority.ADMIN, Authority.OWNER}) // 관리자 및 가게 주인용
     @GetMapping("/user/{userId}")
     public ResponseEntity<ResponseBody<List<AIRecordDto>>> getMyAIRecord(@PathVariable("userId") Long userId) {
-        // TODO. 권한 체크, 예외 처리
+        // TODO. 예외 처리
         List<AI> records = aiService.getMyAIRecord(userId);
         List<AIRecordDto> aiRecords = records.stream()
                 .map(ai -> new AIRecordDto(ai.getRequest(), ai.getResponse()))
